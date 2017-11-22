bb6698ebd4e527f5ef1eb568b81fd41363de8705
gciochina <gciochina@gmail.com>
2017-11-23 01:06:21 +0200

    renamed to dailystatus; version++ todo: encrypt passwords, add edit project

--- Daily.Hours.Web/Properties/AssemblyInfo.cs
+++ Daily.Hours.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Daily.Hours.Web")]
+[assembly: AssemblyTitle("Daily.Status.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("daily.Hours")]
+[assembly: AssemblyProduct("daily.Status")]
 [assembly: AssemblyCopyright("Copyright gciochina©  2005-2040")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.0.3")]
-[assembly: AssemblyFileVersion("0.0.0.3")]
+[assembly: AssemblyVersion("0.0.0.4")]
+[assembly: AssemblyFileVersion("0.0.0.4")]
