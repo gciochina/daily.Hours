c10c4919532c508e04ab9ec79c661b0a7fc08068
gciochina <gciochina@gmail.com>
2017-11-22 21:16:18 +0200

    replace keypress with keyup event to allow knockout to update the observable value before executing the handle

--- Daily.Hours.Web/_spa/scripts/ko.bindingHandlers.js
+++ Daily.Hours.Web/_spa/scripts/ko.bindingHandlers.js
@@ -1,7 +1,7 @@
 ﻿ko.bindingHandlers.onEnter = {
     init: function (element, valueAccessor, allBindings, viewModel) {
         var callback = valueAccessor();
-        $(element).keypress(function (event) {
+        $(element).keyup(function (event) {
             var keyCode = (event.which ? event.which : event.keyCode);
             if (keyCode === 13) {
                 callback.call(viewModel);
